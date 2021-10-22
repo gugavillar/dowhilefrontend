998cfc9b24d97fe067a254e41ae5278d40829548
Guga Villar <grsv.21@gmail.com>
2021-10-22 09:44:16 -0300

    Add variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+const baseURLEnv = String(import.meta.env.VITE_BASE_URL);
 export const api = axios.create({
-  baseURL: 'http://localhost:4000'
+  baseURL: baseURLEnv
 });
